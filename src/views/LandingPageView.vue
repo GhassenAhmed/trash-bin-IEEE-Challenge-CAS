87351ddb42ec59a20e87a4a28ff41bac167e0adb
ghassen <ghassenahmed74@gmail.com>
2023-12-13 15:22:26 +0100

    tes

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -90,5 +90,14 @@ export default {
   color: #F0ECE5;
   font-weight: bold;
 }
-
+.hover03 img{
+	-webkit-transform: scale(1.5);
+	transform: scale(1.5);
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+}
+.hover03 :hover img {
+	-webkit-transform: scale(1);
+	transform: scale(1);
+}
 </style>
