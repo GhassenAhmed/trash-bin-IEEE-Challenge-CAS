5a1d6849af849b47318bc8e64e0dd8bfc894a5ae
ghassen <ghassenahmed74@gmail.com>
2023-12-14 19:29:54 +0100

    add contact

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,15 +3,14 @@
     <v-layout  style="height: 100%;">
       <v-navigation-drawer
         expand-on-hover
-        rail
         style="height: 100%;"
       >
      
         <v-list>
           <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            title="Sandra Adams"
-            subtitle="sandra_a88@gmailcom"
+            prepend-avatar="https://scontent.ftun15-1.fna.fbcdn.net/v/t1.15752-9/371463595_308250398855722_5800845893055358111_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=vgXhNilOcTEAX9IljkD&_nc_ht=scontent.ftun15-1.fna&oh=03_AdT6PWhRS3xbMch3dXG6ZnEDtHZeaktK7UN9i8OMMpwJfg&oe=65A2BF93"
+            title="Ghassen Ahmed"
+            subtitle="ghassenahmed74@gmail"
           ></v-list-item>
         </v-list>
 
@@ -24,11 +23,39 @@
         </v-list>
       </v-navigation-drawer>
 
-      <v-main style="height: 250px"></v-main>
+      <v-main style="height: 250px">
+       dashbord
+      </v-main>
     </v-layout>
-  </v-card>
+    </v-card>
 </template>
 <script>
+  import { use } from "echarts/core";
+  import { CanvasRenderer } from "echarts/renderers";
+  import { PieChart } from "echarts/charts";
+  import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+  } from "echarts/components";
+  import VChart, { THEME_KEY } from "vue-echarts";
+
+  use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+  ]);
+
+  export default {
+    provide: {
+        [THEME_KEY]: "dark"
+    },
+    components:{
+        VChart
+     },
+  }
 </script>
 <style scoped>
 </style>
